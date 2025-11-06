777ecc5c068650e8e2e365f58513bf103d7c143a
MartaPrieto <G00431347@atu.ie>
2025-11-06 17:46:00 +0000

    Initial commit

---
+++ src/main/java/ie/atu/week7jpa/Week7JpaApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.week7jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week7JpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Week7JpaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/week7jpa/Week7JpaApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.week7jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week7JpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
