8e799e3b961461d7c6f79c4e0fa0be22fbfe91dc
MartaPrieto <G00431347@atu.ie>
2025-11-06 18:35:29 +0000

    Added Service and Controller

---
+++ src/main/java/ie/atu/week7jpa/PersonService.java
@@ -0,0 +1,17 @@
+package ie.atu.week7jpa;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PersonService {
+    private final PersonRepository repo;
+    public PersonService(PersonRepository repo) { this.repo = repo; }
+    public Person create(Person p) { return repo.save(p); }
+    public List<Person> findAll() { return repo.findAll(); }
+    public Person findByEmployeeId(String id) {
+        return repo.findByEmployeeId(id)
+                .orElseThrow(() -> new IllegalArgumentException("Person not found"));
+    }
+}
